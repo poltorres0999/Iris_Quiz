58f0bda22031adaef137901005a707999e2579f0
Pol Torres Alfonso <eichgammer@gmail.com>
2019-11-15 01:12:13 +0100

    iris_image model

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     DEBUG = True
     #### Data base configuration ####
-    DB_USERNAME =
-    DB_PASSWORD =
-    DB_NAME =
-    DB_HOST =
-    DB_PORT =
-    SQLALCHEMY_DATABASE_URI = ""
+    DB_USERNAME = "postgres"
+    DB_PASSWORD = "admin"
+    DB_NAME = "iris_webapp_db"
+    DB_HOST = "localhost"
+    DB_PORT = 5432
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
--- models/iris_image.py
+++ models/iris_image.py
@@ -1,9 +1,25 @@
+from app import db
 
 
-class IrisImage:
+class IrisImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    width = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    real = db.Column(db.Boolean, nullable=False)
+    store_path = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, height, width, real, store_path):
-        self.height = height
-        self.width = width
-        self.real = real
-        self.store_path = store_path
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, w):
+        if w < 0: raise ValueError("Width must have a positive value")
+
+    @property
+    def height(self):
+        return self._width
+
+    @height.setter
+    def height(self, h):
+        if h < 0: raise ValueError("Width must have a positive value")
